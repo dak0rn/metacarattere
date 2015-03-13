f2ccb08e137c92c1bbb025ac7c09af8807600c07
dak0rn <daniel@suitsoft.eu>
2015-03-13 13:27:22 +0100

    More helper functions and a cascade pattern

--- index.js
+++ index.js
@@ -26,6 +26,7 @@
     var _replace = String.prototype.replace;
     var _split   = String.prototype.split;
     var _shift   = Array.prototype.shift;
+    var _charAt  = String.prototype.charAt;
 
     var substr = function(str, s, e) {
         return _substr.call(str,s,e);
@@ -43,8 +44,62 @@
         return _shift.call(array);
     };
 
-    // Just return a value to define the module export.
-    // This example returns an object, but the module
-    // can return a function as the exported value.
-    return function(){};
+    var first = function(str) {
+        return _charAt.call(str, 0);
+    };
+
+    var charAt = function(str, pos) {
+        return _charAt.call(str,pos);
+    };
+
+    // Symbols
+    var symbols = {
+        slash: '/',
+        colon: ':',
+
+        multiSlashReplace: /[\/]{2,}/g
+    };
+
+    // Helper functions
+
+    // Returns true if the given string is a placeholder
+    var isPlaceholder = function(what) { return symbols.colon === first(what); };
+
+    // Returns the name of a placeholder
+    var getPlaceholder = function(what) { return substr(what,1); };
+
+    // Normalizes a given URL be removing
+    // leading or trailing slashes and multiple slashes
+    var normalize = function(what) {
+        if( 'string' !== typeof what )
+            return undefined;
+
+        // Remove trailing and leading slashes
+        var st = 0;
+        var en = what.length;
+
+        // Skip leading
+        if( symbols.slash === charAt(what, 0) )
+            st++;
+
+        if( symbols.slash === charAt(what,en-1) )
+            en--;
+
+        what = substr(what,st,en);
+
+        // Replace multiple slashes
+        what = replace(what, symbols.multiSlashReplace, '/');
+
+        return what;
+    };
+
+    // Main function
+    var metacarattere = function(pattern, url) {
+
+    };
+
+    return function(pattern, url){
+        return metacarattere.apply(this,arguments);
+    };
+
 }));
