a8c36e1448618dab7ccae9ca2de5eeb28d6f4da5
dak0rn <daniel@suitsoft.eu>
2015-03-13 12:11:28 +0100

    First test

---
+++ test/all.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var metacarattere = require('../index.js');
+
+describe('Export', function() {
+
+    it('should export a function', function(){
+        expect(metacarattere).to.be.a('function');
+    });
+
+});
+
+describe('Exported function', function() {
+    
+});
