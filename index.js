f56614b7be16e28570b9a2d0f3e98b53e29a1b51
dak0rn <daniel@suitsoft.eu>
2015-03-13 13:05:12 +0100

    Native references included

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * metacarattere URL pattern matcher
  */
 
-
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
@@ -19,6 +18,31 @@
         root.returnExports = factory();
     }
 } (this, function () {
+
+    // References to native functions
+    // This is used to make the script runnable
+    // in environments that changed default functions
+    var _substr = String.prototype.substr;
+    var _replace = String.prototype.replace;
+    var _split   = String.prototype.split;
+    var _shift   = Array.prototype.shift;
+
+    var substr = function(str, s, e) {
+        return _substr.call(str,s,e);
+    };
+
+    var replace = function(str,regex,repl) {
+        return _replace.call(str,regex,repl);
+    };
+
+    var split = function(str, token) {
+        return _split.call(str,token);
+    };
+
+    var shift = function(array) {
+        return _shift.call(array);
+    };
+
     // Just return a value to define the module export.
     // This example returns an object, but the module
     // can return a function as the exported value.
