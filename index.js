a00d20823ccf3b119d1d502f39535cec0fea43c3
dak0rn <daniel@suitsoft.eu>
2015-03-14 12:33:43 +0100

    Banner notice in the main file updated

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 /**
- * metacarattere URL pattern matcher
+ * metacarattere
+ * An URL pattern matcher utility
+ *
+ * Copyright (c) 2015 Daniel Koch
+ *
+ * https://github.com/dak0rn/metacarattere
+ *
+ * Licensed under The MIT License (MIT).
  */
 
 (function (root, factory) {
