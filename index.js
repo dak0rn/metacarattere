f1b1a6f5ecbd4dfbe9541e769394339270f2ab1b
dak0rn <daniel@suitsoft.eu>
2015-03-16 15:31:42 +0100

    A version that passes all the tests

--- index.js
+++ index.js
@@ -70,6 +70,83 @@
     // Returns the name of a placeholder
     var getPlaceholder = function(what) { return substr(what,2); };
 
+    // API functions
+    // These functions are exposed using delegator functions in the
+    // prototype
+    var api = {
+        parse: function(url) {
+            // We want to have a string
+            if( 'string' !== typeof url )
+                return null;
+
+            var values = match(url, this._compiledExpression);
+            var i;
+            var result = {};
+            var len = this._placeholders.length;
+
+            if( null === values )
+                return null;
+
+            // Skip the first value, it contains the whole URL
+            shift(values);
+
+            // Return null, if they did not match the same
+            // number of fields
+            // Should not happen since match() should return `null` in that
+            // case but we really want to be sure
+            if( values.length !== len )
+                return null;
+
+            // Map placeholder names to the values
+            for( i = 0; i < len; i++ )
+                result[ this._placeholders[i] ] = values[i];
+
+            return result;
+        },
+
+        match: function(url) {
+            res = 'string' === typeof url &&
+                null !== match(url, this._compiledExpression);
+        }
+    };
+
+    var metacarattere = function(pattern) {
+
+        // Default values
+        this._compiledExpression = '(?!)';      // Never matching default
+        this._placeholders = [];
+
+        var tmp;
+
+        if( 'string' === typeof pattern ) {
+            // Create an array of placeholder IDs
+            tmp = match(pattern, symbols.placeholder);
+
+            // Parse placeholders
+            if( null !== tmp )
+                this._placeholders = map( tmp, getPlaceholder );
+
+            // Create a regular expression from the pattern
+            this._compiledExpression = replace(pattern, symbols.placeholder, symbols.regexPlaceholder);
+        }
+
+        // Make sure we match full expressions only
+        // If no pattern was given, this creates the expression
+        // $(?!)^ that never matches
+        this._compiledExpression = new RegExp("^" + this._compiledExpression + "$");
+
+    };
+
+    metacarattere.prototype = {
+        match: function(url) {
+            return api.match.apply(this,arguments);
+        },
+
+        parse: function(url) {
+            return api.parse.apply(this,arguments);
+        }
+    };
+
     // Main stuff
     var oldMetacarattere = function( pattern, url ) {
 
@@ -122,8 +199,9 @@
 
     };
 
-    return function(pattern, url){
-        return metacarattere.apply(this,arguments);
-    };
+    // return function(pattern, url){
+    //     return metacarattere.apply(this,arguments);
+    // };
+    return metacarattere;
 
 }));
